6f7bca7a393ab3ed00a803f3198e9777d92d5217
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2024-01-22 12:25:54 +0200

    Install rest_framework and create serializers for model Actor

---
+++ service/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from service.models import Actor
+
+
+class ActorSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField(max_length=63)
+    last_name = serializers.CharField(max_length=63)
+
+    def create(self, validated_data):
+        return Actor.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.save()
+        return instance
+
+
