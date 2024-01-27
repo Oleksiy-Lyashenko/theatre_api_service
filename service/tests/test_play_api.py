80583476b866dac85432da84cce4cd32fa785176
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2024-01-27 13:54:12 +0200

    Change test file

--- service/tests/test_play_api.py
+++ service/tests/test_play_api.py
@@ -1,11 +1,10 @@
-from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
-# from rest_framework import status
+
 from rest_framework.test import APIClient
 
 
-PLAY_URL = reverse("app:service:ticket-list")
+PLAY_URL = reverse("service:play-list")
 
 
 class UnauthenticatedPlayApi(TestCase):
@@ -14,6 +13,6 @@ class UnauthenticatedPlayApi(TestCase):
 
     def test_auth_required(self):
 
-        k = PLAY_URL
+        # k = PLAY_URL
         play = self.client.get(PLAY_URL)
         self.assertEqual(401, 401)
